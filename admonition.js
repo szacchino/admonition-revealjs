3bfd592ab5f0c5551d38c1fad840f3a1fdb9a924
Sandro Zacchino <sandro.zacchino@unisalento.it>
2021-12-06 12:49:28 +0100

    Fix on parsing markdown inside the body of a call-out

--- admonition.js
+++ admonition.js
@@ -1,15 +1,26 @@
 window.RevealAdmonition = {
     id: 'admonition',
     init: function (deck) {
+        // console.log(deck);
         var headings = document.evaluate("//p[starts-with(., '!!!')]", document, null, XPathResult.ANY_TYPE, null );
         var replacements = [];
         if (headings) {
             var oldNode = headings.iterateNext();
             while(oldNode) {
                 var rows = oldNode.textContent.split("\n");
+                // console.log("rows",rows);
                 if (rows.length) {
                     var titles = /(?:!!!)([\w]+)(?:[\s])?(.*)/.exec(rows[0]);
-                    var text = rows[1].trim();
+                    // var titles = rows[0].split('!!!')[1].split(' ');
+                    var text = "";
+                    rows.slice(1).forEach(r => {
+                        // console.log("r ", r);
+                        text += r.replace(/^\s{4}/, "") + "\n"
+                        // console.log(text)
+                    })
+                    // var text = '\n' + rows.slice(1).join('\n').replaceAll;
+                    // console.log(titles);
+                    // console.log('text: ',text);
                     var newNode = document.createElement('div');
                     newNode.classList.add("admonition", titles[1]);
                     if (titles[2] !== '""') {
@@ -19,9 +30,11 @@ window.RevealAdmonition = {
                         newNode.appendChild(newNodeTitle);
                     }                    
                     var newNodePar = document.createElement('p');
-                    newNodePar.innerText = text;
+                    newNodePar.innerHTML = marked.parse(text);
+                    // console.log(newNodePar.innerHTML);
                     newNode.appendChild(newNodePar);
                     replacements.push({oldNode, newNode});
+                    // oldNode.parentNode.replaceChild(newNode, oldNode);
                 }
                 oldNode = headings.iterateNext();
             }
