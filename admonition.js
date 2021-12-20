6b5b83cb0fe79d5ddad7b26257d5c3b06d6fcef1
Sandro Zacchino <sandro.zacchino@unisalento.it>
2021-12-20 16:13:30 +0100

    Fix for Chrome browsers

--- admonition.js
+++ admonition.js
@@ -8,39 +8,31 @@ window.RevealAdmonition = {
             var oldNode = headings.iterateNext();
             while(oldNode) {
                 var rows = oldNode.textContent.split("\n");
-                // console.log("rows",rows);
                 if (rows.length) {
                     var titles = /(?:!!!)([\w]+)(?:[\s])?(.*)/.exec(rows[0]);
-                    // var titles = rows[0].split('!!!')[1].split(' ');
                     var text = "";
                     rows.slice(1).forEach(r => {
-                        // console.log("r ", r);
                         text += r.replace(/^\s{4}/, "") + "\n"
-                        // console.log(text)
                     })
-                    // var text = '\n' + rows.slice(1).join('\n').replaceAll;
-                    // console.log(titles);
-                    // console.log('text: ',text);
-                    var newNode = document.createElement('div');
-                    newNode.classList.add("admonition", titles[1]);
-                    if (titles[2] !== '""') {
-                        var newNodeTitle = document.createElement('p');
-                        newNodeTitle.classList.add('admonition-title');
-                        newNodeTitle.innerText = titles[2] || titles[1].replace(/^\w/, (c) => c.toUpperCase());;
-                        newNode.appendChild(newNodeTitle);
-                    }                    
-                    var newNodePar = document.createElement('p');
-                    newNodePar.innerHTML = marked.parse(text);
-                    // console.log(newNodePar.innerHTML);
-                    newNode.appendChild(newNodePar);
-                    replacements.push({oldNode, newNode});
-                    // oldNode.parentNode.replaceChild(newNode, oldNode);
+                    var ihtml = marked.parse(text);
+                    replacements.push({oldNode, ihtml, titles1: titles[1], titles2: titles[2]});
                 }
                 oldNode = headings.iterateNext();
             }
         }
         replacements.forEach(x => {
-            x.oldNode.parentNode.replaceChild(x.newNode, x.oldNode);
+            var newNode = document.createElement('div');
+            newNode.classList.add("admonition", x.titles1);
+            if (x.titles2 !== '""') {
+                var newNodeTitle = document.createElement('p');
+                newNodeTitle.classList.add('admonition-title');
+                newNodeTitle.innerText = x.titles2 || x.titles1.replace(/^\w/, (c) => c.toUpperCase());;
+                newNode.appendChild(newNodeTitle);
+            }
+            var newNodePar = document.createElement('p');
+            newNodePar.innerHTML = x.ihtml;
+            newNode.appendChild(newNodePar);
+            x.oldNode.parentNode.replaceChild(newNode, x.oldNode);
         }); 
     }
   }
